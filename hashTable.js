7791b31151bea3c897f5a346813510db2eb431eb
Ryan Lai <ryanlai12@gmail.com>
2023-07-12 15:23:31 +0100

    Implemented chaining using linked lists

--- hashTable.js
+++ hashTable.js
@@ -10,20 +10,35 @@ const hashStrToInt = (key, internalArraySize) => {
 
 class HashTable {
 
-  internalArray = new Array(100)
+  internalArray = new Array(5)
 
   setItem = (key,value) => {
     const index = hashStrToInt(key, this.internalArray.length)
-    this.internalArray[index] = value
+
+    if (this.internalArray[index]) {
+      this.internalArray[index].push([key,value])
+    } else {
+      this.internalArray[index] = [[key,value]]
+    }
   }
 
   getItem = (key) => {
     const index = hashStrToInt(key, this.internalArray.length)
-    return this.internalArray[index]
+    const arrayElement = this.internalArray[index]
+
+    if (arrayElement && arrayElement.find(key_value => key_value[0] === key)) {
+      return arrayElement.find(key_value => key_value[0] === key)[1]
+    } else {
+      return 'Key does not exist.'
+    }
   }
 }
 
 const myTable = new HashTable
 myTable.setItem('Hash Table', '哈希表')
+myTable.setItem('Woah', 'wing')
+myTable.setItem('wong', 'wang')
+myTable.setItem('who', 'wang')
 
-console.log(myTable.getItem('Ordinarily'))
+
+console.log(myTable.getItem('Hash Table'))
